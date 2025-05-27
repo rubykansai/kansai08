d43b36e09c20ec67f041871e907114fc9302992d
ydah <t.yudai92@gmail.com>
2025-05-27 16:40:49 +0900

    Add custom chip for Drinkup Sponsor in sponsors page

--- pages/sponsors/index.vue
+++ pages/sponsors/index.vue
@@ -95,6 +95,13 @@
                     </a>
                   </v-col>
                   <v-col>
+                    <v-chip
+                      size="large"
+                      color="#700002"
+                      variant="flat"
+                      class="my-3 px-8"
+                      v-if="content.custom"
+                    >{{ content.custom }}</v-chip>
                     <p>
                       <a
                         class="text-link text-h5 mb-2 name"
@@ -230,6 +237,7 @@ export default {
         url: 'https://engineer.andpad.co.jp/',
         description: 'ANDPADはクラウド型建設プロジェクト管理プラットフォームで、現場効率化から業務改善まで一元管理できます。21万社、55万人の毎日の業務を支え、DX化を加速しています。そして多くのプロダクトでRuby/Railsを使用し、Ruby/Railsが建築・建設業界のDXを支えているとも言えます。アンドパッドではRuby/Railsの良さを駆使し、ユーザーの体験に活かせるRubyistを歓迎します。',
         image: '/kansai08/sponsors/andpad.png',
+        custom: 'Drinkup Sponsor',
       },
       {
         name: '株式会社Degica',
