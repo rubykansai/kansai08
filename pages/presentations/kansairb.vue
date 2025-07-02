57c7913ebe7d72dfd1fe04f1761c4ec10f6461a1
ydah <t.yudai92@gmail.com>
2025-07-02 22:21:56 +0900

    Add communities, panelists, and moderator sections to presentation component

--- pages/presentations/kansairb.vue
+++ pages/presentations/kansairb.vue
@@ -74,6 +74,103 @@
                   <p class="text-body-1 abstract">
                     {{ speaker.abstract }}
                   </p>
+
+                  <v-card
+                    v-if="communities && communities.length > 0"
+                    class="communities-section mt-5 pa-4"
+                    variant="outlined"
+                    color="#700002"
+                  >
+                    <h3 class="text-subtitle-1 mb-3">
+                      <v-icon icon="mdi-account-multiple" size="small" class="mr-2"/>Communities
+                    </h3>
+                    <v-row>
+                      <v-col
+                        v-for="(community, index) in communities"
+                        :key="index"
+                        cols="12"
+                        sm="6"
+                        md="4"
+                      >
+                        <a
+                          :href="community.url"
+                          target="_blank"
+                          class="community-link"
+                        >
+                          <v-card
+                            class="community-card pa-3"
+                            variant="tonal"
+                            color="#700002"
+                          >
+                            <v-card-text class="d-flex align-center">
+                              <v-icon
+                                icon="mdi-ruby"
+                                size="small"
+                                class="mr-2"
+                              />
+                              <span class="text-body-2">
+                                {{ community.name }}
+                                <v-icon
+                                  icon="mdi-open-in-new"
+                                  size="x-small"
+                                  class="ml-auto"
+                                />
+                                </span>
+                            </v-card-text>
+                          </v-card>
+                        </a>
+                      </v-col>
+                    </v-row>
+                  </v-card>
+
+                  <v-card
+                    v-if="panelists && panelists.length > 0"
+                    class="panelists-section mt-5 pa-4"
+                    variant="outlined"
+                    color="#700002"
+                  >
+                    <h3 class="text-subtitle-1 mb-3">
+                      <v-icon icon="mdi-account-group" size="small" class="mr-2"/>Panelists
+                    </h3>
+                    <v-list density="compact" bg-color="transparent">
+                      <v-list-item
+                        v-for="(panelist, index) in panelists"
+                        :key="index"
+                        class="px-0"
+                      >
+                        <template v-slot:prepend>
+                          <v-avatar size="40">
+                            <v-img :src="panelist.image" />
+                          </v-avatar>
+                        </template>
+                        <v-list-item-title>{{ panelist.name }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ panelist.affiliation }}</v-list-item-subtitle>
+                      </v-list-item>
+                    </v-list>
+                  </v-card>
+
+                  <v-card
+                    v-if="moderator"
+                    class="moderator-section mt-5 pa-4"
+                    variant="outlined"
+                    color="#700002"
+                  >
+                    <h3 class="text-subtitle-1 mb-3">
+                      <v-icon icon="mdi-microphone" size="small" class="mr-2"/>Moderator
+                    </h3>
+                    <v-list density="compact" bg-color="transparent">
+                      <v-list-item class="px-0">
+                        <template v-slot:prepend>
+                          <v-avatar size="40">
+                            <v-img :src="moderator.image" />
+                          </v-avatar>
+                        </template>
+                        <v-list-item-title>{{ moderator.name }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ moderator.affiliation }}</v-list-item-subtitle>
+                      </v-list-item>
+                    </v-list>
+                  </v-card>
+
                   <v-divider class="my-5" />
                   <a
                     v-if="speaker.slide"
@@ -107,6 +204,96 @@ export default {
       title: 'Regional.rb and the Kyoto City',
       abstract: '関西の地域.rbのオーガナイザーが関西Ruby会議に集結！',
       type: 'panel',
+    },
+    communities: [
+      { name: 'AKASHI.rb', url: 'https://akashi-rb.connpass.com/' },
+      { name: 'Hirakata.rb', url: 'https://hirakatarb.connpass.com/' },
+      { name: 'KOBE.rb', url: 'https://koberb.doorkeeper.jp/' },
+      { name: 'Kyobashi.rb', url: 'https://kyobashirb.connpass.com/' },
+      { name: 'Kyoto.rb', url: 'https://kyotorb.connpass.com/' },
+      { name: 'naniwa.rb', url: 'https://naniwarb.doorkeeper.jp/' },
+      { name: 'Ruby関西', url: 'https://rubykansai.doorkeeper.jp/' },
+      { name: 'Ruby舞鶴', url: 'https://ruby-maizuru.connpass.com/' },
+      { name: 'Ruby Tuesday', url: 'https://ruby-tuesday.doorkeeper.jp/' },
+      { name: 'Shinosaka.rb', url: 'https://shinosakarb.doorkeeper.jp/' },
+      { name: 'Wakayama.rb', url: 'https://wakayama-rb.connpass.com/' },
+    ],
+    panelists: [
+      {
+        name: '無双',
+        affiliation: 'AKASHI.rb',
+        image: 'https://avatars.githubusercontent.com/u/46985135?v=4'
+      },
+      {
+        name: 'sago35',
+        affiliation: 'AKASHI.rb',
+        image: 'https://avatars.githubusercontent.com/u/9251039?v=4'
+      },
+      {
+        name: 'sanfrecce_osaka',
+        affiliation: 'Hirakata.rb',
+        image: 'https://avatars.githubusercontent.com/u/7955461?v=4'
+      },
+      {
+        name: 'youcune',
+        affiliation: 'KOBE.rb',
+        image: 'https://avatars.githubusercontent.com/u/2869221?v=4'
+      },
+      {
+        name: 'hachi',
+        affiliation: 'Kyobashi.rb',
+        image: 'https://avatars.githubusercontent.com/u/8371588?v=4'
+      },
+      {
+        name: 'ydah',
+        affiliation: 'Kyobashi.rb',
+        image: 'https://avatars.githubusercontent.com/u/13041216?v=4'
+      },
+      {
+        name: 'luccafort',
+        affiliation: 'Kyoto.rb',
+        image: 'https://avatars.githubusercontent.com/u/1229937?v=4'
+      },
+      {
+        name: 'onk',
+        affiliation: 'Kyoto.rb',
+        image: 'https://avatars.githubusercontent.com/u/203046?v=4'
+      },
+      {
+        name: 'おごもり',
+        affiliation: 'Ruby関西/naniwa.rb',
+        image: 'https://avatars.githubusercontent.com/u/471923?v=4'
+      },
+      {
+        name: 'znz',
+        affiliation: 'Ruby関西',
+        image: 'https://avatars.githubusercontent.com/u/11857?v=4'
+      },
+      {
+        name: '107steps',
+        affiliation: 'Ruby舞鶴',
+        image: 'https://avatars.githubusercontent.com/u/2397400?v=4'
+      },
+      {
+        name: 'toshima66',
+        affiliation: 'Ruby Tuesday',
+        image: 'https://avatars.githubusercontent.com/u/904191?v=4'
+      },
+      {
+        name: 'むらじゅん',
+        affiliation: 'Shinosaka.rb',
+        image: 'https://avatars.githubusercontent.com/u/911903?v=4'
+      },
+      {
+        name: 'たろサ',
+        affiliation: 'Wakayama.rb',
+        image: 'https://avatars.githubusercontent.com/u/1458076?v=4'
+      },
+    ],
+    moderator: {
+      name: 'pastak',
+      affiliation: 'Kyoto.js',
+      image: 'https://avatars.githubusercontent.com/u/191533'
     },
   }),
 }
@@ -153,6 +340,12 @@ export default {
   font-weight: 500;
 }
 
+.text-subtitle-1 {
+  color: #700002;
+  font-weight: 600;
+  font-size: 16px;
+}
+
 .text-body-1 {
   line-height: 1.7;
 }
@@ -193,6 +386,37 @@ svg.v-icon__svg {
 
 .slide-btn .v-icon {
   margin-right: 7px;
+}
+
+.moderator-section,
+.panelists-section,
+.communities-section {
+  border-color: rgba(112, 0, 2, 0.2) !important;
+  background-color: rgba(254, 240, 240, 0.5);
+}
+
+.community-card {
+  background-color: rgba(112, 0, 2, 0.08) !important;
+  transition: all 0.2s ease-in-out;
+}
+
+.community-link {
+  text-decoration: none;
+  color: inherit;
+}
+
+.community-link:hover .community-card {
+  background-color: rgba(112, 0, 2, 0.15) !important;
+  transform: translateY(-2px);
+}
+
+.v-list-item {
+  min-height: 48px;
+}
+
+.v-list-item-subtitle {
+  font-size: 12px;
+  opacity: 0.8;
 }
 
 @media (max-width: 600px) {
