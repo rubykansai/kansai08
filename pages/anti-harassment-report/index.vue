07a2918a36e51b4fc2c4999d07c414b5c04c50ba
ydah <t.yudai92@gmail.com>
2025-07-23 14:33:33 +0900

    Add anti-harassment report page with apology and contact information

---
+++ pages/anti-harassment-report/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-app class="fill-height">
+    <AppHeader />
+    <v-row>
+      <v-col align="center" class="pb-0 mt-12">
+        <v-card
+          max-width="60vw"
+          min-width="60vw"
+          class="pt-10 pb-10 my-15 px-15 d-none d-md-block"
+          :elevation="0"
+        >
+          <v-card-item>
+            <div align="left">
+              <h1 class="text-h4 title align-center mb-10">
+                関西Ruby会議08の本編においてのアンチハラスメントポリシーに抵触する行為に関するお詫びとご報告
+              </h1>
+              <p class="text-body-1 mb-5">
+                関西Ruby会議08にご参加いただいた皆様へ
+              </p>
+              <p class="text-body-1 mb-5">
+                今回の関西Ruby会議08本編において、アンチハラスメントポリシーに抵触する行動があったとの報告を受けました。
+              </p>
+              <p class="text-body-1 mb-5">
+                我々としてはこの事態を重く受けとめており、ハラスメントの被害にあった方との事実確認を行い、事実関係を確認いたしました。当該行為は、参加者の容姿に関する配慮に欠けるコミュニケーションであり、これは明確にアンチハラスメントポリシーに違反するものです。今後の再発防止に向けて、関係者に通告をいたしました。
+              </p>
+              <p class="text-body-1 mb-5">
+                参加者の皆様におかれましては、全員が安心して楽しくイベントに参加できるよう、アンチハラスメントポリシーに則った行動をしていただきますよう、改めてお願い申し上げます。
+              </p>
+              <p class="text-body-1 mb-5">
+                この度はこのような事態が起こってしまったことを深くお詫び申し上げます。
+              </p>
+              <p class="text-body-1 mb-5">
+                他にも被害にあった、現場を見たなどの報告がございましたら、<a href="mailto:kansai08@rubykaigi.org">kansai08@rubykaigi.org</a> までご連絡ください。
+              </p>
+              <p class="text-body-1 mb-5">
+                関西Ruby会議08実行委員会一同
+              </p>
+            </div>
+          </v-card-item>
+        </v-card>
+        <v-card
+          max-width="90vw"
+          min-width="90vw"
+          class="my-10 d-block d-md-none"
+          :elevation="0"
+        >
+          <v-card-item>
+            <div align="left">
+              <h1 class="text-h4 title align-center mb-10">
+                関西Ruby会議08の本編においてのアンチハラスメントポリシーに抵触する行為に関するお詫びとご報告
+              </h1>
+              <p class="text-body-1 mb-5">
+                関西Ruby会議08にご参加いただいた皆様へ
+              </p>
+              <p class="text-body-1 mb-5">
+                今回の関西Ruby会議08本編において、アンチハラスメントポリシーに抵触する行動があったとの報告を受けました。
+              </p>
+              <p class="text-body-1 mb-5">
+                我々としてはこの事態を重く受けとめており、ハラスメントの被害にあった方との事実確認を行い、事実関係を確認いたしました。当該行為は、参加者の容姿に関する配慮に欠けるコミュニケーションであり、これは明確にアンチハラスメントポリシーに違反するものです。今後の再発防止に向けて、関係者に通告をいたしました。
+              </p>
+              <p class="text-body-1 mb-5">
+                参加者の皆様におかれましては、全員が安心して楽しくイベントに参加できるよう、アンチハラスメントポリシーに則った行動をしていただきますよう、改めてお願い申し上げます。
+              </p>
+              <p class="text-body-1 mb-5">
+                この度はこのような事態が起こってしまったことを深くお詫び申し上げます。
+              </p>
+              <p class="text-body-1 mb-5">
+                他にも被害にあった、現場を見たなどの報告がございましたら、<a href="mailto:kansai08@rubykaigi.org">kansai08@rubykaigi.org</a> までご連絡ください。
+              </p>
+              <p class="text-body-1 mb-5">
+                関西Ruby会議08実行委員会一同
+              </p>
+            </div>
+          </v-card-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+
+<script setup>
+useSeoMeta({
+  title: '関西Ruby会議08の本編においてのアンチハラスメントポリシーに抵触する行為に関するお詫びとご報告',
+  ogTitle: '関西Ruby会議08の本編においてのアンチハラスメントポリシーに抵触する行為に関するお詫びとご報告',
+})
+</script>
+
+<style scoped>
+* {
+  font-family: 'Noto Sans JP';
+  box-sizing: border-box;
+}
+
+.v-application {
+  color: #700002;
+  position: relative;
+}
+
+.v-card-item {
+  color: #222222;
+}
+
+.dot {
+  width: 20px;
+  height: 20px;
+}
+
+.title {
+  font-weight: 400;
+  display: flex;
+}
+
+.text-h4 {
+  color: #700002;
+  font-weight: 500;
+}
+
+.text-h5 {
+  color: #700002;
+  font-weight: 600;
+}
+
+.text-h6 {
+  color: #700002;
+}
+
+.text-body-1 {
+  line-height: 1.7;
+  font-family: "Noto Sans JP";
+}
+</style>
